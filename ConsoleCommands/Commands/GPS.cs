1952604baf952989b5232276022e26c36e2f3c01
CubeSuite <cubesuite@outlook.com>
2024-07-11 23:28:33 +0100

    Added missing files

---
+++ ConsoleCommands/Commands/GPS.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCommands
+{
+    internal static partial class Commands {
+        internal static Command gps = new Command() {
+            name = "gps",
+            description = "Triggers a notification with your current position",
+            examples = new List<string>() { "gps" },
+            Execute = () => {
+                CommandManager.Notify(Player.instance.transform.position.ToString());
+            }
+        };
+    }
+}
